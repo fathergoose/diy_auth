6564b774322a3169aea30e65fde3c24d93a45b82
fathergoose <al@ilseman.com>
2016-05-21 11:41:15 -0500

    routing users, changed order!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :users
+
+  get 'signup' => 'users#new'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
