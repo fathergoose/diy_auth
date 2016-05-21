8bedb8bbd86b94094a06e6522a4ea8e2af9f3624
fathergoose <al@ilseman.com>
2016-05-21 11:28:28 -0500

    valid posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,14 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "post is invalid without title and content" do
+    invalid_post = Post.new title: nil, content: nil
+    assert_not invalid_post.valid?
+  end
+
+  test "post can be saved with title and content" do
+    post = Post.new title: posts(:one).title, content: posts(:one).content  
+    assert post.valid?
+  end
+
 end
